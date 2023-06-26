6c44d31bc4c4a015b67b2858a73a14f88ce5bcef
Simon Damerow <simon.damerow@student.uibk.ac.at>
2023-06-26 08:17:44 +0200

    Simon: locationerror bug gefixt

--- main.js
+++ main.js
@@ -206,8 +206,13 @@ map.on('locationfound', function (evt) {
 }
 );
 
+var errorDisplayed = false;
+
 map.on('locationerror', function (evt) {
-    alert(evt.message);
+    if (!errorDisplayed) {
+        alert(evt.message);
+        errorDisplayed = true;
+    }
 });
 
 // //GPX-Track visualisieren -> Höhenprofile (es sind noch nicht alle)
